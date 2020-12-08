d799f7e664cb6c6c54b099f2fce56bfea82e25ed
pawan deore <pavandeore90@gmail.com>
2020-12-08 12:52:20 +0530

    print functionality working

--- execute_command.py
+++ execute_command.py
@@ -1,6 +1,12 @@
 def execute_fun(command) : 
     keyword = command.split()[0]
-    
+    remaining_words = " ".join(command.split()[1:])
+
     if keyword == 'exit':
-        print('\nExiting Application.. ')
+        return '\nExiting Application.. '
         exit()
+
+    if keyword == 'print':
+        formated_remaining_word = "('{}')".format(remaining_words)
+        print("\nOutput ->")
+        return exec(keyword+formated_remaining_word)
