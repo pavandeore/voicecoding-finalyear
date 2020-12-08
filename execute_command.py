eaebae331cae3c5030f86bd51d980df88887b213
pawan deore <pavandeore90@gmail.com>
2020-12-08 13:20:19 +0530

    variable assign functionality added

--- execute_command.py
+++ execute_command.py
@@ -10,3 +10,10 @@ def execute_fun(command) :
         formated_remaining_word = "('{}')".format(remaining_words)
         print("\nOutput ->")
         return exec(keyword+formated_remaining_word)
+    
+    if keyword == 'assign':
+        value = remaining_words.split()[0]
+        variable = remaining_words.split()[3]
+        formated = '{} = {}'.format(variable,value)
+        print(formated)
+        return exec(formated)
